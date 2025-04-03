a7d8a013008c6313767470a0563a1f0bafa0004d
kruvv <kruvv@mail.ru>
2025-04-03 17:21:35 +0300

    Создал приложение и добавил форму для добавления постов

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <section>
+        <h1>Feed of posts</h1>
+
+        <div className="navContent">
+          <div className="navLinks"></div>
+        </div>
+      </section>
+    </nav>
+  )
+}
