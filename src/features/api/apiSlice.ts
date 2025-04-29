84e118bcbe5083fda2e22a6672aa3709a9ae9fa0
kruvv <kruvv@mail.ru>
2025-04-29 12:06:43 +0300

    Добавил новый срез для добавление rtk query

---
+++ src/features/api/apiSlice.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type { Post } from '../posts/postsSlice'
+export type { Post }
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  endpoints: (builder) => ({
+    getPost: builder.query<Post[], void>({
+      query: () => '/posts',
+    }),
+  }),
+})
+
+export const { useGetPostsQuery } = apiSlice
